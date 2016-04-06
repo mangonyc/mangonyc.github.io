b20f919c45cb16bdb8a4cbb01322878298e5a9af
Chris Melvin <Christopher.Melvin@stmartins.com>
2016-04-06 15:00:17 -0400

    this should be the final project

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
+// Everything happens in one main function, that function is instantiated upon the button click
 $('#myButt').on('click', function() {
     var picslist = $('#the-pics')
+    var msg = $('.msg')
     picslist.empty();
+    msg.empty().css('border', 'none');
+
     var query = $('#mySearch').val();
-
-    if (query == '') alert('please fuck off');
-
-    console.log(query);
-
     var url = 'https://api.spotify.com/v1/search';
+    var myAudio;
 
+    if (query == '') {
+        msg.css('border', '1px solid #c00')
+        msg.html('C’mon, you can think of something')
+    }
+// This is the main API call, utilizing the value of the search input field as its query
     $.ajax({
             url: url,
             data: {
                 q: query,
                 type: 'album'
             },
+            // for each artist we are going to dig down to extract the albums, their titles, their cover images-using [1] which is the middle picture available at 300px wide. We create DOM elements for each and append them to a div on the page
             success: function(artist) {
-                    // console.log(artist.albums.items);
                     var albums = artist.albums.items;
                     albums.forEach(function(album) {
                         $('#mySearch').val('');
-                        // console.log(album.name);
                         albumurl = album.images[1].url;
                         albumname = album.name;
-                        // console.log(album);
-                        // debugger
-
-
                         var theCover = $('<img class="g-flip" src="' + albumurl + '"/>');
                         var $newDiv = $('<div class="indiv-album">')
                         var itemContainer = $('<div class="containr">');
@@ -35,6 +35,7 @@ $('#myButt').on('click', function() {
                         $newDiv.data('albumlink', album.href);
 
                         $newDiv.append(itemContainer);
+                        // This GSAP code sets the album divs to an opacity of 0, and a scale of 2x, so they come in from larger and fade in.
                         TweenMax.set($newDiv, {
                             opacity: 0,
                             scale: 2
@@ -43,6 +44,7 @@ $('#myButt').on('click', function() {
                         itemContainer.append(theCover);
                         itemContainer.data('albumlink', album.href);
                         $newDiv.append($newTitle)
+                        // This GSAP code takes the divs with album covers and titles in them, and rotates the divs into the page.
                         TweenMax.staggerTo($('.indiv-album'), 1, {
                             opacity: 1,
                             scale: 1,
@@ -50,80 +52,116 @@ $('#myButt').on('click', function() {
                             transformPerspective: 1800,
                             ease: Power3.easeIn
                         }, .16);
-					    theCover.on('click', function(e) {
-					    	console.log('did it click');
-					        var $album = $(this);
-					        // closeAllAlbums()
-					        turnAlbum($album);
-					    })
+                        // This click function carries over the parameter $album and will spin the cover 180° through the turnAlbum() function - it only rotates the cover- theCover represents the img of the cover - this is important becuase the title and eventually the tracklist need to be at 0° to be read correctly.
+                        theCover.on('click', function(e) {
+                            console.log('did it click');
+                            var $album = $(this);
+                            // closeAllAlbums()
+                            turnAlbum($album);
+                        })
                     });
-                    
+
                 } //end success from main json
         }) //end ajax command
 
- 
-    // get tracklist
+
+    // This function takes the album which as this point is being passed as the img of the album cover- the tracklist data resides two levels in the div outside the img, so we need to set it to the outer div by using parent().parent(). Now we can access the tracklist.
+    function turnAlbum($album) {
+        console.log("turn album");
+        var topAlbum = $album.parent().parent()
+        var trackItems = $('.mytracks , .flippy');
+        trackItems.remove();
+        //  find all the albums except the one you flipped
+        var restOfAlbums = topAlbum.siblings().find('.g-flip');
+        // set all the rest of the albums to 0° before turning so that only one album is turned at a time
+        TweenMax.to(restOfAlbums, 1, {
+            rotationY: 0,
+            ease: Power3.ease0ut
+        });
+        //  This GSAP creates a timeline that will turn the album 180° and sets a function to load the tracklist to be run at the completion of the tween.
+        var tl = new TimelineMax({
+            onComplete: function() {
+                getTracklist($album)
+            }
+        });
+        tl.to($album, 1.5, {
+            opacity: 1,
+            scale: 1,
+            rotationY: 180,
+            transformPerspective: 1800,
+            ease: Power4.easeOut
+        });
+    } // end of turnAlbum
+
+    // This function loads the tracklist after an album is turned.
     function getTracklist($album) {
-        console.log('get tracklist');
+        // reaassign the $album variable to the top level div which contains the tracklist API url.
         $album = $album.parent().parent();
-        console.log($album);
         $album.addClass('tracksOpen');
         var tracklistUrl = $album.data('albumlink');
-        console.log(tracklistUrl);
         var $itemContainer = $album.find('.containr')
-
-        // console.log('TRACKLISTURL $$$$$$ ', tracklistUrl);
-
-        // console.log(tracklistUrl);
         var tracklist = $('<ul class="mytracks">')
 
+        // Here we call the tracklist API, add a "FLIP" button to the tracklist in case the user just wants to close that album without interacting with any others.
         $.get(tracklistUrl, function(trax) {
                 var traxNames = trax.tracks.items;
-                // console.log(trax.tracks.items[0].preview_url);
                 var flipBtn = $('<button class="flippy">FLIP ME BACK</button>');
                 $itemContainer.append(tracklist);
                 $itemContainer.append(flipBtn);
 
-                // get the track names for each album
-
+                // get the track names and preview urls for each album.
                 traxNames.forEach(function(track) {
                     var trackname = track.name;
                     var listenUrl = track.preview_url;
-                    // console.log(listenUrl);
                     var $newLi = $('<li class="track-name">');
                     $newLi.html(trackname);
-                    // $newLi.html('<a href="' + listenUrl + '">' + trackname + '</a>');
+                    // set the track names opacity to 0, so they can fade in.
                     TweenMax.set($newLi, {
                         opacity: 0
                     });
                     $newLi.css('cursor', 'pointer');
                     tracklist.append($newLi);
                     $itemContainer.append(tracklist);
+                    // This GSAP fades in the tracknames right after one another.
                     TweenMax.staggerTo($('.track-name'), .5, {
                         opacity: 1,
                         scale: 1,
                         ease: Power3.easeIn
                     }, .076);
                     $itemContainer.data('tracklink', listenUrl);
-                    // console.log(listenUrl);
-                    // add functionality to preview the track
-                    // $newLi.click(function(e) {
-                    //  var playing = 'playing';
-                    //  var target = e.target;
-                    //  var myAudio = new Audio(listenUrl);
-                    //  // check to see if the track is already playing
-
-                    //     if (myAudio.paused && myAudio.currentTime > 0 && !myAudio.ended) {
-                    //         console.log('audio play');
-                    //               myAudio.pause();
-                    //     } else {
-                    //         console.log('audio pause');
-                    //               myAudio.play();
-                    //     }
-                    // });
+                    //  Create a click event for each track name to play/pause it.
+                    $newLi.on('click', function(e) {
+                        // create a class to add to atrack when played to identify it later to be turned off.
+                        var playingTrack = 'playing'
+                        var target = e.target;
+                        if (target !== null) {
+                            // Check to see if the track is playing already by class name, if it is, pause it becuase you are playing a new track.
+                            if ($(this).hasClass('playing')) {
+                                console.log('just pause it');
+                                myAudio.pause();
+                            } else {
+                                // otherise the track exists already- we should pause that too, most likely clicking on the same track to pause it.
+                                if (myAudio) {
+                                    myAudio.pause();
+                                    console.log('my second pause');
+                                }
+                                console.log('play it');
+                                // Create a new audio object now, and assign it the preview url and play it, add the "playing" class
+                                myAudio = new Audio(listenUrl)
+                                myAudio.play();
+                                target.classList.add(playingTrack);
+                                myAudio.addEventListener('ended', function() {
+                                    target.classList.remove(playingTrack);
+                                });
+                                myAudio.addEventListener('pause', function() {
+                                    target.classList.remove(playingTrack);
+                                });
+                            }
+                        }
+                    });
                 })
 
-                // the button to flip the album cover back
+                // the button to flip the album cover back to allow the original state of the album to be achieved.
                 flipBtn.on('click', flipButton);
 
                 function flipButton() {
@@ -143,44 +181,5 @@ $('#myButt').on('click', function() {
             }) // end of $.get trax
             // console.log('track list ran');
     } //end of getTracklist
-
-    function turnAlbum($album) {
-        console.log("turn album");
-        // console.log($album.parent().parent());
-        var topAlbum = $album.parent().parent()
-        var stuff = $('.mytracks , .flippy');
-        stuff.remove();
-        var nx = topAlbum.siblings().find('.g-flip');
-        // console.log(nx);
-        TweenMax.to(nx, 1, {
-                rotationY: 0,
-                ease: Power3.ease0ut
-            });
-            // console.log($albumImg);
-            // var tl = new TimelineMax()
-        var tl = new TimelineMax({
-                onComplete: function() {
-                    getTracklist($album)
-                }
-            });
-        tl.to($album, 1.5, {
-            opacity: 1,
-            scale: 1,
-            rotationY: 180,
-            transformPerspective: 1800,
-            ease: Power4.easeOut
-        });
-        // myAlbum.each(function(){           
-        // if ($(this).hasClass('tracksOpen')) {
-        //     console.log($(this));
-        //     var $albumImg = $(this).find('.g-flip');
-        //     flipButton();
-        // } else {
-        // var $albumImg = $album.find('.g-flip');
-        // // console.log($albumImg);
-        // }
-        // });
-    } // end of turnAlbum
-
-    // theCover.click(turnAlbum);
+    
 });
